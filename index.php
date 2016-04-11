c9e756d9cce62329929c81bd486c606b198f9461
GautierMichelin <gautier.michelin@gmail.com>
2016-04-11 23:06:53 +0200

    Adding a bunch of CORS headers

--- index.php
+++ index.php
@@ -3,6 +3,23 @@
 $dsn = '';
 $clients = [];
 
+// Allow from any origin
+	header("Access-Control-Allow-Origin: *");
+	header('Access-Control-Allow-Credentials: true');
+	header('Access-Control-Max-Age: 86400');    // cache for 1 day
+
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+	if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+		header("Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE, PUT");
+
+	if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+		header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+	exit(0);
+}
+
 /**
 * The MIT License
 * http://creativecommons.org/licenses/MIT/
